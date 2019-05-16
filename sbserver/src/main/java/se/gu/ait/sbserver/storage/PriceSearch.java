b5992a2190e2e6a415061109967d9d17a4291010
EllinorStomdal <ellinor.stomdal@gmail.com>
2019-05-16 09:41:04 +0100

    ändringar i PriceSearch

--- sbserver/src/main/java/se/gu/ait/sbserver/storage/PriceSearch.java
+++ sbserver/src/main/java/se/gu/ait/sbserver/storage/PriceSearch.java
@@ -7,8 +7,9 @@ public class ProductSearch{
     DBHelper getConn = new DBHelper();
   public static ResultSet nrSearch() {
     try {
-      PreparedStatement preparedStatment = getConn.createStatement();
-      
+      PreparedStatement nrSearch = getConn.prepareStatement(
+      "SELECT * FROM priceChanges WHERE nr= ? ;");
+
     } catch(Exception e) {
 
     }
